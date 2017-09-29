82f41e99e1821a6df7fb036a79e0da7707c53f62
laomafeima <laomafeima@gmail.com>
2017-09-29 17:21:31 +0800

    feat: A light wrapper around SQLite

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from sqlight import version
+
+setup(
+    name="sqlight",
+    version=version,
+    description="A lightweight wrapper around SQLite.",
+    long_description='A lightweight wrapper around SQLite. just on Python3',
+    keywords='sqlite sqlite3 lightweight wrapper',
+    author='laoma',
+    url='https://github.com/laomafeima/sqlight',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    py_modules=["sqlight"],
+    )
